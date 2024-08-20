63d5fadafa4be292030f5be6c28b1c298e545b25
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-08-20 19:55:52 +0500

    typeError

--- resume/serializers.py
+++ resume/serializers.py
@@ -137,7 +137,7 @@ class JobSerializer(serializers.ModelSerializer):
     job_start_date = serializers.SerializerMethodField(method_name="get_field_custom")
     job_end_date = serializers.SerializerMethodField(method_name="get_field_custom")
 
-    @extend_schema_field(OpenApiTypes.DATETIME)
+    @extend_schema_field(OpenApiTypes.DATE)
     def get_field_custom(self, object):
         return "DD-MM-YYYY YYYY-MM-DD"
 
