da517a78709508f27ac1b1701dbaeb3fd7812829
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-08-21 19:38:45 +0500

    error

--- paractice_module/views.py
+++ paractice_module/views.py
@@ -8,43 +8,43 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from paractice_module.serializers import (
-    EducationSerializer_Paractice_Request,
-    EducationSerializer_Paractice_Response,
+    # EducationSerializer_Paractice_Request,
+    # EducationSerializer_Paractice_Response,
     EducationSerializer_Paractice_Field_Request,
     EducationSerializer_Paractice_Field_Response,
 )
 
 
-@extend_schema(
-    request=EducationSerializer_Paractice_Request,
-    responses={"201": EducationSerializer_Paractice_Response},
-    methods=["POST"],
-)
-class EducationCreateAPIView(APIView):
-    """
-    API view to create a new Education entry.
-    Paractice of @extend_schema_serializer decorator
-    """
+# @extend_schema(
+#     request=EducationSerializer_Paractice_Request,
+#     responses={"201": EducationSerializer_Paractice_Response},
+#     methods=["POST"],
+# )
+# class EducationCreateAPIView(APIView):
+#     """
+#     API view to create a new Education entry.
+#     Paractice of @extend_schema_serializer decorator
+#     """
 
-    parser_classes = [JSONParser]
-    renderer_classes = [JSONRenderer]
-    throttle_classes = [CustomAnonRateThrottle]
-    http_method_names = ["post", "options"]
+#     parser_classes = [JSONParser]
+#     renderer_classes = [JSONRenderer]
+#     throttle_classes = [CustomAnonRateThrottle]
+#     http_method_names = ["post", "options"]
 
-    def post(self, request, format=None):
-        serializer = EducationSerializer_Paractice_Request(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+#     def post(self, request, format=None):
+#         serializer = EducationSerializer_Paractice_Request(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
 
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def finalize_response(self, request, response, *args, **kwargs):
-        response = super().finalize_response(request, response, *args, **kwargs)
+#     def finalize_response(self, request, response, *args, **kwargs):
+#         response = super().finalize_response(request, response, *args, **kwargs)
 
-        response["X-RateLimit-Limit"] = request.rate_limit["X-RateLimit-Limit"]
-        response["X-RateLimit-Remaining"] = request.rate_limit["X-RateLimit-Remaining"]
-        return response
+#         response["X-RateLimit-Limit"] = request.rate_limit["X-RateLimit-Limit"]
+#         response["X-RateLimit-Remaining"] = request.rate_limit["X-RateLimit-Remaining"]
+#         return response
 
 
 @extend_schema(
