bd79191a5016105804506543b3a6f85bda17e21e
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-08-22 14:44:11 +0500

    roll back paractice_module changes

--- paractice_module/serializers.py
+++ paractice_module/serializers.py
@@ -1,95 +1,47 @@
 from rest_framework import serializers
-from paractice_module.custom_fields import CustomDateField
-from datetime import datetime, timedelta
+
+# from paractice_module.custom_fields import CustomDateField
+# from datetime import datetime, timedelta
 from drf_spectacular.utils import (
-    extend_schema_field,
+    # extend_schema_field,
     extend_schema_serializer,
     OpenApiExample,
-    OpenApiTypes,
+    # OpenApiTypes,
 )
 from resume.models import Education
 
 
-# @extend_schema_serializer(
-#     exclude_fields=("id",),
-#     examples=[
-#         OpenApiExample(
-#             name="Example 1",
-#             summary="short summary",
-#             description="longer description",
-#             value={
-#                 "name": "eight",
-#                 "location": "North Jon",
-#                 "schoolurl": "https://www.stone-logan.com/",
-#                 "education_start_date": {
-#                     "01-01-2020": True,
-#                     "2021-01-31": True,
-#                 },
-#                 "education_end_date": {
-#                     "01-01-2020": True,
-#                     "2021-01-31": True,
-#                 },
-#                 "degree": "degree",
-#                 "description": "Onto production back. Response gun read child.",
-#             },
-#             request_only=True,
-#             response_only=False,
-#         ),
-#     ],
-# )
-# class EducationSerializer_Paractice_Request(serializers.ModelSerializer):
+@extend_schema_serializer(
+    exclude_fields=("id",),
+    examples=[
+        OpenApiExample(
+            name="Example 1",
+            summary="short summary",
+            description="DateField can accept DD-MM-YYY, YYYY-MM-DD",
+            value={
+                "name": "eight",
+                "location": "North Jon",
+                "schoolurl": "https://www.stone-logan.com/",
+                "education_start_date": {
+                    "01-01-2020": True,
+                    "2021-01-31": True,
+                },
+                "education_end_date": {
+                    "01-01-2020": True,
+                    "2021-01-31": True,
+                },
+                "degree": "degree",
+                "description": "DateField can accept DD-MM-YYY, YYYY-MM-DD",
+            },
+            request_only=True,
+            response_only=False,
+        ),
+    ],
+)
+class EducationSerializer_Paractice_Request(serializers.ModelSerializer):
 
-#     education_start_date = serializers.DateField(input_formats=["%d-%m-%Y"])
-#     education_end_date = serializers.DateField(input_formats=["%d-%m-%Y"])
-
-#     class Meta:
-#         model = Education
-#         fields = [
-#             "name",
-#             "location",
-#             "schoolurl",
-#             "education_start_date",
-#             "education_end_date",
-#             "degree",
-#             "description",
-#         ]
-
-#     def update(self, instance, validated_data):
-#         fields_to_update = [
-#             "name",
-#             "location",
-#             "schoolurl",
-#             "education_start_date",
-#             "education_end_date",
-#             "degree",
-#             "description",
-#         ]
-
-#         for field in fields_to_update:
-#             setattr(
-#                 instance, field, validated_data.get(field, getattr(instance, field))
-#             )
-#             instance.save()
-
-#         return instance
-
-
-# class EducationSerializer_Paractice_Response(EducationSerializer_Paractice_Request):
-
-#     education_start_date = serializers.DateField(format="%Y-%m-%d")
-#     education_end_date = serializers.DateField(format="%Y-%m-%d")
-
-
-class EducationSerializer_Paractice_Field_Request(serializers.ModelSerializer):
-    # Use the custom date field with specific date formats
-    education_start_date_ = CustomDateField(
-        input_formats=["%d-%m-%Y", "%Y-%m-%d", "%m/%d/%Y"],
-        default=lambda: datetime.now().strftime("%d-%m-%Y"),
-    )
-    education_end_date_ = CustomDateField(
-        input_formats=["%d-%m-%Y", "%Y-%m-%d", "%m/%d/%Y"],
-        default=lambda: (datetime.now() + timedelta(days=7)).strftime("%d-%m-%Y"),
-    )
+    education_start_date = serializers.DateField(input_formats=["%d-%m-%Y", "%Y-%m-%d"])
+    education_end_date = serializers.DateField(input_formats=["%d-%m-%Y", "%d-%m-%Y"])
 
     class Meta:
         model = Education
@@ -97,8 +49,8 @@ class EducationSerializer_Paractice_Field_Request(serializers.ModelSerializer):
             "name",
             "location",
             "schoolurl",
-            "education_start_date_",
-            "education_end_date_",
+            "education_start_date",
+            "education_end_date",
             "degree",
             "description",
         ]
@@ -123,9 +75,58 @@ class EducationSerializer_Paractice_Field_Request(serializers.ModelSerializer):
         return instance
 
 
-class EducationSerializer_Paractice_Field_Response(
-    EducationSerializer_Paractice_Field_Request
-):
+class EducationSerializer_Paractice_Response(EducationSerializer_Paractice_Request):
 
-    education_start_date_ = serializers.DateField(format="%Y-%m-%d")
-    education_end_date_ = serializers.DateField(format="%Y-%m-%d")
+    education_start_date = serializers.DateField(format="%Y-%m-%d")
+    education_end_date = serializers.DateField(format="%Y-%m-%d")
+
+
+# class EducationSerializer_Paractice_Field_Request(serializers.ModelSerializer):
+#     # Use the custom date field with specific date formats
+#     education_start_date_ = CustomDateField(
+#         input_formats=["%d-%m-%Y", "%Y-%m-%d", "%m/%d/%Y"],
+#         default=lambda: datetime.now().strftime("%d-%m-%Y"),
+#     )
+#     education_end_date_ = CustomDateField(
+#         input_formats=["%d-%m-%Y", "%Y-%m-%d", "%m/%d/%Y"],
+#         default=lambda: (datetime.now() + timedelta(days=7)).strftime("%d-%m-%Y"),
+#     )
+
+#     class Meta:
+#         model = Education
+#         fields = [
+#             "name",
+#             "location",
+#             "schoolurl",
+#             "education_start_date_",
+#             "education_end_date_",
+#             "degree",
+#             "description",
+#         ]
+
+#     def update(self, instance, validated_data):
+#         fields_to_update = [
+#             "name",
+#             "location",
+#             "schoolurl",
+#             "education_start_date",
+#             "education_end_date",
+#             "degree",
+#             "description",
+#         ]
+
+#         for field in fields_to_update:
+#             setattr(
+#                 instance, field, validated_data.get(field, getattr(instance, field))
+#             )
+#             instance.save()
+
+#         return instance
+
+
+# class EducationSerializer_Paractice_Field_Response(
+#     EducationSerializer_Paractice_Field_Request
+# ):
+
+#     education_start_date_ = serializers.DateField(format="%Y-%m-%d")
+#     education_end_date_ = serializers.DateField(format="%Y-%m-%d")
--- paractice_module/views.py
+++ paractice_module/views.py
@@ -4,58 +4,27 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+
+# from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from paractice_module.serializers import (
-    # EducationSerializer_Paractice_Request,
-    # EducationSerializer_Paractice_Response,
-    EducationSerializer_Paractice_Field_Request,
-    EducationSerializer_Paractice_Field_Response,
+    EducationSerializer_Paractice_Request,
+    EducationSerializer_Paractice_Response,
+    # EducationSerializer_Paractice_Field_Request,
+    # EducationSerializer_Paractice_Field_Response,
 )
-
-
-# @extend_schema(
-#     request=EducationSerializer_Paractice_Request,
-#     responses={"201": EducationSerializer_Paractice_Response},
-#     methods=["POST"],
-# )
-# class EducationCreateAPIView(APIView):
-#     """
-#     API view to create a new Education entry.
-#     Paractice of @extend_schema_serializer decorator
-#     """
-
-#     parser_classes = [JSONParser]
-#     renderer_classes = [JSONRenderer]
-#     throttle_classes = [CustomAnonRateThrottle]
-#     http_method_names = ["post", "options"]
-
-#     def post(self, request, format=None):
-#         serializer = EducationSerializer_Paractice_Request(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def finalize_response(self, request, response, *args, **kwargs):
-#         response = super().finalize_response(request, response, *args, **kwargs)
-
-#         response["X-RateLimit-Limit"] = request.rate_limit["X-RateLimit-Limit"]
-#         response["X-RateLimit-Remaining"] = request.rate_limit["X-RateLimit-Remaining"]
-#         return response
 
 
 @extend_schema(
-    request=EducationSerializer_Paractice_Field_Request,
-    responses={"201": EducationSerializer_Paractice_Field_Response},
+    request=EducationSerializer_Paractice_Request,
+    responses={"201": EducationSerializer_Paractice_Response},
     methods=["POST"],
 )
-class EducationCreateAPIView_Schema_Field(APIView):
+class EducationCreateAPIView(APIView):
     """
     API view to create a new Education entry.
-    Paractice of @extend_schema_field decorator, but failed
+    Paractice of @extend_schema_serializer decorator
     """
 
     parser_classes = [JSONParser]
@@ -64,7 +33,7 @@ class EducationCreateAPIView_Schema_Field(APIView):
     http_method_names = ["post", "options"]
 
     def post(self, request, format=None):
-        serializer = EducationSerializer_Paractice_Field_Request(data=request.data)
+        serializer = EducationSerializer_Paractice_Request(data=request.data)
         if serializer.is_valid():
             serializer.save()
 
@@ -77,3 +46,35 @@ class EducationCreateAPIView_Schema_Field(APIView):
         response["X-RateLimit-Limit"] = request.rate_limit["X-RateLimit-Limit"]
         response["X-RateLimit-Remaining"] = request.rate_limit["X-RateLimit-Remaining"]
         return response
+
+
+# @extend_schema(
+#     request=EducationSerializer_Paractice_Field_Request,
+#     responses={"201": EducationSerializer_Paractice_Field_Response},
+#     methods=["POST"],
+# )
+# class EducationCreateAPIView_Schema_Field(APIView):
+#     """
+#     API view to create a new Education entry.
+#     Paractice of @extend_schema_field decorator, but failed
+#     """
+
+#     parser_classes = [JSONParser]
+#     renderer_classes = [JSONRenderer]
+#     throttle_classes = [CustomAnonRateThrottle]
+#     http_method_names = ["post", "options"]
+
+#     def post(self, request, format=None):
+#         serializer = EducationSerializer_Paractice_Field_Request(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def finalize_response(self, request, response, *args, **kwargs):
+#         response = super().finalize_response(request, response, *args, **kwargs)
+
+#         response["X-RateLimit-Limit"] = request.rate_limit["X-RateLimit-Limit"]
+#         response["X-RateLimit-Remaining"] = request.rate_limit["X-RateLimit-Remaining"]
+#         return response
